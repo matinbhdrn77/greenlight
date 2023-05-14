8f95e18e5adad8287cd43c2777d4927aa43b371f
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-14 12:04:41 +0330

    create custom Runtime struct so we can encode it in more flexible way

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,7 +7,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	RunTime   int32     `json:"runtime,omitempty"`
+	RunTime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int       `json:"version"`
 }
