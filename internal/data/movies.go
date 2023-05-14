347c9243ed3702f800465f89bd07b229878b2d2c
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-14 12:15:32 +0330

    define MarshalJSON on movie so we can have more flexibility in marshaling fields

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
@@ -10,4 +14,24 @@ type Movie struct {
 	RunTime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int       `json:"version"`
+}
+
+func (m Movie) MarshalJSON() ([]byte, error) {
+	var runtime string
+
+	if m.RunTime != 0 {
+		runtime = fmt.Sprintf("%d mins", m.RunTime)
+	}
+
+	type MovieAlias Movie
+
+	aux := struct {
+		MovieAlias
+		Runtime string
+	}{
+		MovieAlias: MovieAlias(m),
+		Runtime:    runtime,
+	}
+
+	return json.Marshal(aux)
 }
