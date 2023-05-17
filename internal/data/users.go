c12d68661d48982b29f9022e9259dcc96f265ec0
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-17 10:58:18 +0330

    methods for setinng and matching hash passwords

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,6 +1,11 @@
 package data
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID        int64     `json:"id"`
@@ -13,6 +18,34 @@ type User struct {
 }
 
 type password struct {
+	// pointer so we can distinguish between a plaintext password
+	// not being present in the struct at all, versus a empty string""
 	plaintext *string
 	hash      []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
 }
