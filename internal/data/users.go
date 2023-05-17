797fd1363b659d8a4fcc13e87440f11f68ae742a
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-17 11:17:11 +0330

    validate client input for creating new user

--- internal/data/users.go
+++ internal/data/users.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/matinbhdrn77/greenlight/internal/validator"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -22,6 +23,32 @@ type password struct {
 	// not being present in the struct at all, versus a empty string""
 	plaintext *string
 	hash      []byte
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
 
 func (p *password) Set(plaintextPassword string) error {
