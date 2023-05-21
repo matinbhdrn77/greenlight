05da97ff7c1f324d3aad170cf23f9d233994d753
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-05-21 12:48:37 +0330

    middleware for authenticating user requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/matinbhdrn77/greenlight/internal/data"
+	"github.com/matinbhdrn77/greenlight/internal/validator"
 	"golang.org/x/time/rate"
 )
 
@@ -88,6 +92,51 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 			mu.Unlock()
 
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// any caches that the response may vary based on the value
+		// of the Authorization header in the request.
+		w.Header().Add("Vary", "Authorization")
+
+		authorizationHeader := r.Header.Get("Authorization")
+		if authorizationHeader == "" {
+			r = app.contextSetUser(r, data.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		headersParts := strings.Split(authorizationHeader, " ")
+		if len(headersParts) != 2 || headersParts[0] != "Bearer" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		token := headersParts[1]
+
+		v := validator.New()
+
+		if data.ValidateTokenPlainText(v, token); !v.Valid() {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		user, err := app.models.Users.GetForToken(data.ScopeAuthentication, token)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+
+		r = app.contextSetUser(r, user)
+
 		next.ServeHTTP(w, r)
 	})
 }
